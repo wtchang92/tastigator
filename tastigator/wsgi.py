2fab51ae05e28af4641e4a57e82d06228b0dc7db
wtchang92 <wtchang051592@gmail.com>
2016-11-10 23:47:15 -0500

    heroku fix

--- tastigator/wsgi.py
+++ tastigator/wsgi.py
@@ -7,10 +7,17 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
 
+# import os
+#
+# from django.core.wsgi import get_wsgi_application
+#
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tastigator.settings")
+#
+# application = get_wsgi_application()
+
+
 import os
-
 from django.core.wsgi import get_wsgi_application
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tastigator.settings")
-
-application = get_wsgi_application()
+from dj_static import Cling
+application = Cling(get_wsgi_application())
